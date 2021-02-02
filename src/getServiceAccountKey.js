e8437ac4655ea4d493d43b8efbeaac1569733d88
Jaime <jaimeloeuf@gmail.com>
2021-02-02 15:54:47 +0800

    Expand scope of getServiceAccountKey:     Expand the ways user can set the service account key or its location     for the function to find and return as an object.

--- src/getServiceAccountKey.js
+++ src/getServiceAccountKey.js
@@ -1,29 +1,37 @@
 /**
  * When running on non-gcp cloud provider infra, use this to read service account key either from ENV or file
  * @function getServiceAccountKey
+ * @returns {Object} service account key object
  */
 module.exports = function getServiceAccountKey() {
-  // If inside env var return it after parsing
-  if (process.env.serviceAccountKey)
+  // If env var for default credentials set, and is a JSON string, parse and return
+  if (
+    process.env.GOOGLE_APPLICATION_CREDENTIALS &&
+    process.env.GOOGLE_APPLICATION_CREDENTIALS.charAt(0) === "{"
+  )
+    return JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+  // If inside env var, and is a JSON string, parse and return
+  if (
+    process.env.serviceAccountKey &&
+    process.env.serviceAccountKey.charAt(0) === "{"
+  )
     return JSON.parse(process.env.serviceAccountKey);
-  else {
-    /**
-     * Else read the file directly
-     * Usually when running service locally
-     * Service Key json to be placed in root dir/
-     */
-    const serviceAccountFile = require("path").join(
-      __dirname,
-      "../../serviceAccountKey.json"
-    );
 
-    // Inner import as only used conditionally
-    const fs = require("fs");
+  /**
+   * Else usually when running service locally
+   *
+   * Use path specified in env var to find service account key file,
+   * Else assuming in node_modules dir, traverse up to get file that should be placed in root dir
+   */
+  const serviceAccountFilePath =
+    process.env.serviceAccountKeyPath ||
+    require("path").join(__dirname, "../serviceAccountKey.json");
 
-    if (fs.existsSync(serviceAccountFile)) return require(serviceAccountFile);
-    else
-      throw new Error(
-        `Service Account Key file not available locally at: "${serviceAccountFile}"`
-      );
-  }
+  // Inner import of fs module as only used conditionally
+  // Use require to load and parse file into an object
+  if (require("fs").existsSync(serviceAccountFilePath))
+    return require(serviceAccountFilePath);
+  else
+    throw new Error(`Service Account Key not at: "${serviceAccountFilePath}"`);
 };
