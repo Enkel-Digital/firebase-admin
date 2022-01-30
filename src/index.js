7031e21463f67ba4d51fe109a944f462e5e16781
Jaime <jaimeloeuf@gmail.com>
2022-01-31 00:09:30 +0800

    Export auth and firestore service from index.js:     So that lib users can easily access those services using this same     wrapper module instead of having to import auth and firestore service     from the firebase-admin module directly again, making this wrapper     a unified API for users.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 /**
- * Module wrapper around firebase-admin to initialize it based on the platform we run it on.
+ * Module wrapper around firebase-admin.
+ * 1. To initialize firebase based on the platform we run it on.
+ * 2. Create and export default service objects for the most commonly used modules, firestore and auth.
  * @author JJ
  * @module Firebase Admin initialized app singleton
  */
 
+// Import initializeApp to export it and the default firebase app
 const initializeApp = require("./initializeApp.js");
 module.exports = initializeApp();
 module.exports.initializeApp = initializeApp;
+
+// Import getFirestore to export it and the default firestore service
+const { getFirestore } = require("firebase-admin/firestore");
+module.exports.fs = getFirestore();
+module.exports.getFirestore = getFirestore;
+
+// Import getAuth to export it and the default auth service
+const { getAuth } = require("firebase-admin/auth");
+module.exports.auth = getAuth();
+module.exports.getAuth = getAuth;
